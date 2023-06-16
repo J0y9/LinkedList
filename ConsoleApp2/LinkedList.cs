f8d9db46cfeef4bca6fd7c9cad0a6db7a1d1c544
Jafar <jaafaroday22@gmail.com>
2023-06-16 11:11:29 +0300

    Add project files.

---
+++ ConsoleApp2/LinkedList.cs
@@ -0,0 +1,98 @@
+﻿namespace ConsoleApp2;
+
+public class Node
+{
+    public  string Data { get; set; }
+    public Node? Next { get; set; }
+}
+public  class LinkedList
+{
+    public Node Head { get; set; }
+    public void AddFirst(string data)
+    {
+        var newNode = new Node();
+
+        newNode.Data = data;
+        newNode.Next = Head;
+        Head = newNode;
+    }
+
+    public void AddLast(string data)
+    {
+        if (Head == null)
+        {
+            Head = new Node();
+            Head.Data = data;
+            Head.Next = null;
+
+        }
+        else
+        {
+            var newNode = new Node();
+            newNode.Data = data;
+
+            var current = Head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            current.Next = newNode;
+        }
+    }
+    public void PrintLinkedList()
+    {
+        Node current = Head;
+        while (current != null)
+        {
+            Console.WriteLine(current.Data);
+            current = current.Next;
+        }
+    }
+
+    public string GetIndex(string data)
+    {
+        if (Head == null)
+        {
+            return "Empty Linked List";
+        }
+
+        var index = 0;
+        Node current = Head;
+        while (current != null)
+        {
+            if (current.Data == data)
+            {
+                return $"index of {data} is {index}";
+                    
+            }
+            current = current.Next;
+            index++;
+        }
+
+        return "No Node exist with this data";
+    }
+    public string IsExist(string data)
+    {
+        if (Head == null)
+        {
+            return "Empty Linked List";
+        }
+
+
+        Node current = Head;
+        while (current != null)
+        {
+            if (current.Data == data)
+            {
+                return "element is exist";
+
+            }
+            current = current.Next;
+
+        }
+
+        return "No Node exist with this data";
+    }
+
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp2;
+
+LinkedList l = new LinkedList();
+l.AddFirst("Python");
+l.AddFirst("JS");
+l.AddFirst("C#");
+l.PrintLinkedList();
+
+//Q17
+Console.WriteLine(l.GetIndex("Python"));
+
+//Q18
+Console.WriteLine(l.IsExist("C#"));
+//
